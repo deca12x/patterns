399d1927bd6a8b2d85c6dc649a7ed15e009a3a8a
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-03-29 18:31:25 +0000

    verifier.t.sol

--- test/verifier.t.sol
+++ test/verifier.t.sol
@@ -3,51 +3,33 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "forge-std/Vm.sol";
-import {MedicinePurchase} from "../src/pharmacy.sol";
 import {Verifier} from "../src/verifier.sol";
 
 contract PharmacyTest is Test {
     // Variable for contract instance
-    MedicinePurchase private pharmacy;
-    Verifier internal verifier;
+    Verifier private verifier;
     address internal deployer;
     ErrorsTest test;
 
     function setUp() public {
         deployer = msg.sender;
         verifier = new Verifier();
-        pharmacy = new MedicinePurchase(address(verifier));
-        test = new ErrorsTest();
     }
 
     function testLogAddresses() public {
-        emit log("Pharmacy contract address:");
-        emit log_address(address(pharmacy));
         emit log("Verifier contract address:");
         emit log_address(address(verifier));
-        emit log("Pharmacy Test address:");
+        emit log("Verifier Test address:");
         emit log_address(address(this));
     }
 
     function testGetPharmacyOwner() public {
-        assertEq(pharmacy.owner(), address(this), "ok");
-        // assertTrue(pharmacy.owner() == address(this));
+        // assertEq(verifier.verifyTx(myBool, myInput), 1, "ok");
     }
-
-    // function testSetA() public {
-    //     address newOwner = address(verifier);
-    //     pharmacy.store(newOwner);
-    //     assertTrue(a.retrieve() == 123);
-    // }
 
     // function testExpectArithmetic() public {
     //     vm.expectRevert(stdError.arithmeticError);
     //     test.arithmeticError(10);
-    // }
-
-    // function testFoo(uint256 x) public {
-    //     vm.assume(x < type(uint128).max);
-    //     assertEq(x + x, x * 2);
     // }
 }
 
