9b1fb0299cf8f7b0a2f6874224b655c55a0e4d18
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-03-30 19:31:44 +0000

    testWithdraw done

--- test/pharmacy.t.sol
+++ test/pharmacy.t.sol
@@ -131,6 +131,54 @@ contract PharmacyTest is Test {
         vm.stopPrank();
     }
 
+    function buyMedicineA() public {
+        IVerifier.Proof memory proof1 = IVerifier.Proof({
+            a: IVerifier.G1Point(
+                0x27045bd3cf96298d09188bf83231adf5b13236e8002d9d2e864513dada6ab47d,
+                0x25f0bfe8e6401d9a0498ecec861a70ba32b11dac1a807722dfa615fe17887e83
+            ),
+            b: IVerifier.G2Point(
+                [
+                    0x0059eceab5e351f4e5b2c0a628eda4edef5ede5cdce2a900edde56da881a0fc8,
+                    0x16dc8f37ff35b12973538dba8915afa617a4a4d040240ef6ee5abf7aaf0dfe32
+                ],
+                [
+                    0x128d78f20a7b8650fd5e38b6b5a0808a517e5f2b7237f467fd0bcc2d38c69860,
+                    0x0e16e2df1a65030bb88fc84e9862b12a0707dd89c7faf73bd928450a07c62c69
+                ]
+            ),
+            c: IVerifier.G1Point(
+                0x19b56485bc972765ebd2114539533508c1efca42943b247c584711c208843ec5,
+                0x026219f3dc394f6f587cbc1056fe765d7a6bfc64cdadd00e35a9614bab217dcb
+            )
+        });
+        address payable address1 = payable(
+            0x974BFC05C4B51d4B9d84131A9A870EEcCFB77121
+        );
+
+        vm.deal(address1, 1 ether);
+        vm.startPrank(address1);
+        pharmacy.buyMedicineA{value: 1e15}(proof1);
+        vm.stopPrank();
+    }
+
+    receive() external payable {}
+
+    function testWithdraw() public {
+        vm.deal(address(this), 1 ether);
+        pharmacy.withdraw();
+
+        address payable address1 = payable(
+            0x974BFC05C4B51d4B9d84131A9A870EEcCFB77121
+        );
+
+        vm.deal(address1, 1 ether);
+        vm.startPrank(address1);
+        vm.expectRevert();
+        pharmacy.withdraw();
+        vm.stopPrank();
+    }
+
     // function testSetA() public {
     //     address newOwner = address(verifier);
     //     pharmacy.store(newOwner);
