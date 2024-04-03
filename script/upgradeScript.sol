99484dc3c54d2cdd9bc209340a8139fdad89cad9
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-04-03 22:33:50 +0100

    trying to set up upgrade plugin

---
+++ script/upgradeScript.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+// address proxy = Upgrades.deployUUPSProxy(
+//     "pharmacy.sol",
+//     abi.encodeCall(MyContract.initialize, ("arguments for the initialize function"))
+// );
