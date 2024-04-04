fb49619559cd4ab3f931325448d9b6a9342fb1b5
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-04-04 02:16:48 +0100

    trying to push to main

--- scripts/upgrade.ts
+++ scripts/upgrade.ts
@@ -7,12 +7,14 @@ async function main() {
   const signers = await ethers.getSigners();
   const firstSignerAddress: string = signers[0].address;
 
+  // DEPLOY VERIFIER CONTRACT
   const verifierFactory = await ethers.getContractFactory("Verifier");
   const verifier = await verifierFactory.deploy();
   const verifierReceipt = await verifier.waitForDeployment();
   const verifierAddress = await verifier.getAddress();
   console.log("Verifier deployed to:", verifierAddress);
 
+  // DEPLOY PHARMACY IMPLEMENTATION CONTRACT WITH OPENZEPPELIN UPGRADES PLUGIN
   const pharmacyFactory = await ethers.getContractFactory("MedicinePurchase");
   const pharmacy = await upgrades.deployProxy(pharmacyFactory, [
     verifierAddress,
